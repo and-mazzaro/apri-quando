284c3e1408606dc04be464894fb5aba8170d8f24
and-mazzaro <andreamazzaro8@gmail.com>
2025-07-30 12:26:01 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,61 +2,68 @@ function apri(tipo) {
   const messaggi = {
   "triste": `Allora amore, so che in questo momento sei triste, so che è un momento difficile ma io sono qua, qua per aiutarti, ascoltarti o semplicemente restare in silenzio finché non sarai pronta a parlarne. 
 
-Sono sicuro che, se me ne vuoi parlare, troveremo una soluzione insieme, in questo momento magari rappresenta tipo una montagna da scalare, possiamo superare questa montagna insieme…
+  Sono sicuro che, se me ne vuoi parlare, troveremo una soluzione insieme, in questo momento magari rappresenta tipo una montagna da scalare, possiamo superare questa montagna insieme…
 
-Se per caso qualcuno ti ha fatto qualcosa gli vado sotto casa io, se è successo qualcosa che non doveva succedere possiamo trovare un nuovo modo per farla andare bene oppure trovare qualcosa che ti faccia stare meglio, lo vuoi un abbraccio? ti do anche un bacino se vuoi… Sono qua per te e ci sarò sempre. 
+  Se per caso qualcuno ti ha fatto qualcosa gli vado sotto casa io, se è successo qualcosa che non doveva succedere possiamo trovare un nuovo modo per farla andare bene oppure trovare qualcosa che ti faccia stare meglio, lo vuoi un abbraccio? ti do anche un bacino se vuoi… Sono qua per te e ci sarò sempre. 
 
-Se mari sono via o altro ricordati di quelle facce da coglione che facevo oppure di quei momenti dove non la smettevamo di ridere, ripensa a quando eravamo felici, sperando che questi ti strappi un sorriso o una risata, ti voglio un mondo di bene amoreee, sono qui per te quando ti senti pronta   
-`,
+  Se mari sono via o altro ricordati di quelle facce da coglione che facevo oppure di quei momenti dove non la smettevamo di ridere, ripensa a quando eravamo felici, sperando che questi ti strappi un sorriso o una risata, ti voglio un mondo di bene amoreee, sono qui per te quando ti senti pronta   
+  `,
 
-  "felice": `Sono felicissimo per te amore, meriti tutta la felicità del mondo, sono la persona più felice al mondo quando ti vedo sorridere, non lasciarti mai abbattere dalle altre persone, so che sembra la tipica cosa che dici, “ma sì dai dice così perchè è la tipica frase fatta” e invece no ahahahah, di solito diamo molto peso a cose che non hanno senso. 
+    "felice": `Sono felicissimo per te amore, meriti tutta la felicità del mondo, sono la persona più felice al mondo quando ti vedo sorridere, non lasciarti mai abbattere dalle altre persone, so che sembra la tipica cosa che dici, “ma sì dai dice così perchè è la tipica frase fatta” e invece no ahahahah, di solito diamo molto peso a cose che non hanno senso. 
 
-Detto ciò voglio che tu ti goda questo momento al massimo, non te ne devi perdere un secondo, sei ancora qua? 
+  Detto ciò voglio che tu ti goda questo momento al massimo, non te ne devi perdere un secondo, sei ancora qua? 
 
-Vai a divertirti amore, poi raccontami tutto. 
-Hai un sorriso splendido… sono contentissimo che ti sei divertita o ti stai divertendo, non so in che momento leggerai questo messaggio ma sapere che lo aprirai nei momenti in cui sei felice o anche a caso, mi rende felice anche a me. 
+  Vai a divertirti amore, poi raccontami tutto. 
+  Hai un sorriso splendido… sono contentissimo che ti sei divertita o ti stai divertendo, non so in che momento leggerai questo messaggio ma sapere che lo aprirai nei momenti in cui sei felice o anche a caso, mi rende felice anche a me. 
 
-Perchè si è vero, quando sei felice te sono felice anche io, quando io sono felice anche tu lo sei, e una cosa che viene trasmessa la felicità, soprattutto quando sarai triste (oltre a leggere il messaggio che c’è di là) sarò sempre qua a scherzare, ridere, fare il coglione per strapparti un sorriso o una risata, in quel momento la priorità sarà renderti felice.   
-`,
+  Perchè si è vero, quando sei felice te sono felice anche io, quando io sono felice anche tu lo sei, e una cosa che viene trasmessa la felicità, soprattutto quando sarai triste (oltre a leggere il messaggio che c’è di là) sarò sempre qua a scherzare, ridere, fare il coglione per strapparti un sorriso o una risata, in quel momento la priorità sarà renderti felice.   
+  `,
 
 
-  "hai-dubbi": `Allora, era da un po' che volevo scriverti questa cosa, perché, perché si, fatto sta che questo mi sembra il momento migliore, sono due mesi che stiamo insieme e sinceramente mi sto divertito un mondo, mi trovo benissimo con te, e ti giuro che io una persona migliore di te non la potevo trovare;
-Lo so lo ripeto molto spesso il punto e che è vero, sei unica, perché sei la prima che c'è veramente stata per me, quella che sta sveglia anche tutta la notte se sa che stai male, quella che ti fa ridere anche nei momenti no, quella persona super affettuosa, ma anche super attiva. 
-Tu rappresenti la perfezione c'è poco da dire.
-Ogni tanto, molto spesso, mi fermo e mi rendo conto di quanto sono fortunato ad averti nella mia vita, e a quanto questa cosa mi sta facendo bene, allora mi fermo guardando il vuoto pensando, con la faccia di uno che sta guardando la cosa più bella nella sua vita, penso a te e dico cazzo che fortuna.
-Adoro tutto di te, dai momenti tristi, a quelli felici, o quando non finiamo più di ridere, quando ci pestiamo, gli sguardi improvvisi che ogni tanto ci scambiamo prima di metterci a ridere, i discorsi profondi, la determinazione nel portare a termine un progetto, i momenti in cui ti preoccupi o sei interessata a qualcosa, i momenti dove eravamo io e te e il resto del mondo sembrava isolato.
-Ma anche di fisico come gli occhi, il tuo naso, le orecchie che con tutti gli orecchini stai da dio, le tue coscie, e si anche tette e culo non le togliamo dall'elenco; Il tuo fisico non ti piace lo so eppure io sono convinto del contrario, sono convinto per come sei sei perfetta, e io sarò qua a ricordartelo in ogni momento, quando ti sentirai insicura oppure timorosa, io sarò qua a dirti che sei perfetta.
-Si l'ho dico da tutto il messaggio (perfetta) però fammi aggiungere altro all'elenco...
-C'è anche la parte del carattere, divertente, affettuosa, gelosa, determinata, un po' testarda ma va passo passo con la determinazione(non è una cosa negativa amore), sai sempre dare consigli magnifici, sei disposta a tutto per tutti, in più sei provocatoria, sensibile, ma anche agguerrita, forte, spavalda, intelligente, innovativa, curiosa, con spirito critico eee molto altri che in questi momento non mi viene in mente.
-Di sicuro avrò saltato qualcosa maaa tutto questo era per dirti che mi colori le giornate, con te sto bene e ho grandi progetti per noi, mi aspettavo qualcosa di così lungo? Sì ahahahhs.
-A parte gli scherzi non so come dirti che sei perfetta, e che io con te sto benissimo, e questo mi sembrava un modo efficace  per dirtelo.
-Ti amo tantissimo amoreeee e ti voglio tanto bene 💋💋💋💋
-`,
+    "hai-dubbi": `Allora, era da un po' che volevo scriverti questa cosa, perché, perché si, fatto sta che questo mi sembra il momento migliore, sono due mesi che stiamo insieme e sinceramente mi sto divertito un mondo, mi trovo benissimo con te, e ti giuro che io una persona migliore di te non la potevo trovare;
+  Lo so lo ripeto molto spesso il punto e che è vero, sei unica, perché sei la prima che c'è veramente stata per me, quella che sta sveglia anche tutta la notte se sa che stai male, quella che ti fa ridere anche nei momenti no, quella persona super affettuosa, ma anche super attiva. 
+  Tu rappresenti la perfezione c'è poco da dire.
+  Ogni tanto, molto spesso, mi fermo e mi rendo conto di quanto sono fortunato ad averti nella mia vita, e a quanto questa cosa mi sta facendo bene, allora mi fermo guardando il vuoto pensando, con la faccia di uno che sta guardando la cosa più bella nella sua vita, penso a te e dico cazzo che fortuna.
+  Adoro tutto di te, dai momenti tristi, a quelli felici, o quando non finiamo più di ridere, quando ci pestiamo, gli sguardi improvvisi che ogni tanto ci scambiamo prima di metterci a ridere, i discorsi profondi, la determinazione nel portare a termine un progetto, i momenti in cui ti preoccupi o sei interessata a qualcosa, i momenti dove eravamo io e te e il resto del mondo sembrava isolato.
+  Ma anche di fisico come gli occhi, il tuo naso, le orecchie che con tutti gli orecchini stai da dio, le tue coscie, e si anche tette e culo non le togliamo dall'elenco; Il tuo fisico non ti piace lo so eppure io sono convinto del contrario, sono convinto per come sei sei perfetta, e io sarò qua a ricordartelo in ogni momento, quando ti sentirai insicura oppure timorosa, io sarò qua a dirti che sei perfetta.
+  Si l'ho dico da tutto il messaggio (perfetta) però fammi aggiungere altro all'elenco...
+  C'è anche la parte del carattere, divertente, affettuosa, gelosa, determinata, un po' testarda ma va passo passo con la determinazione(non è una cosa negativa amore), sai sempre dare consigli magnifici, sei disposta a tutto per tutti, in più sei provocatoria, sensibile, ma anche agguerrita, forte, spavalda, intelligente, innovativa, curiosa, con spirito critico eee molto altri che in questi momento non mi viene in mente.
+  Di sicuro avrò saltato qualcosa maaa tutto questo era per dirti che mi colori le giornate, con te sto bene e ho grandi progetti per noi, mi aspettavo qualcosa di così lungo? Sì ahahahhs.
+  A parte gli scherzi non so come dirti che sei perfetta, e che io con te sto benissimo, e questo mi sembrava un modo efficace  per dirtelo.
+  Ti amo tantissimo amoreeee e ti voglio tanto bene 💋💋💋💋
+  `,
 
-    "primo-mese": `
-Ti devo dire la verità, questo mese non l’ho sentito per niente mi sembra ieri che e partito tutto;
-E si, ammetto che la prima volta che ti ho visto non mi aspettavo tutto questo, più che altro perchè sono una persona difficile da amare o in generale da capire, non sono una delle classiche persone che trovi ogni giorno, ma questa e anche uno dei miei pregi come credo che avrai notato.
+      "primo-mese": `
+  Ti devo dire la verità, questo mese non l’ho sentito per niente mi sembra ieri che e partito tutto;
+  E si, ammetto che la prima volta che ti ho visto non mi aspettavo tutto questo, più che altro perchè sono una persona difficile da amare o in generale da capire, non sono una delle classiche persone che trovi ogni giorno, ma questa e anche uno dei miei pregi come credo che avrai notato.
 
-La prima volta che ti ho vista “mi hai sorpresa”, ti ho sempre detto così quando mi chiedevi perchè mi sono convinto di provarci con te… beh tutto quello che ti ho detto è vero, ma ci sono cose che non ho mai detto a nessuno tipo della notte insonne, subito dopo che Sam mi ha messo la verità in faccia, passata a guardare il soffitto e a capire se ne valeva la pena e devo dire che appena ho messo da parte le mie paranoie e mi sono convinto ho capito che dovevo metterci tutto il mio impegno;
+  La prima volta che ti ho vista “mi hai sorpresa”, ti ho sempre detto così quando mi chiedevi perchè mi sono convinto di provarci con te… beh tutto quello che ti ho detto è vero, ma ci sono cose che non ho mai detto a nessuno tipo della notte insonne, subito dopo che Sam mi ha messo la verità in faccia, passata a guardare il soffitto e a capire se ne valeva la pena e devo dire che appena ho messo da parte le mie paranoie e mi sono convinto ho capito che dovevo metterci tutto il mio impegno;
 
-Durante questo mese ho capito varie cose, per prima cosa ho capito che una ragazza migliore di così non la trovo più, perchè in questo mese mi hai illuminato le giornate, fin da subito mi hai dato consigli e mi sei stata vicino in molti momenti difficili, questo non è da tutti molti mi avrebbero mandato a fanculo molto tempo fa ma tu sei rimasta.
+  Durante questo mese ho capito varie cose, per prima cosa ho capito che una ragazza migliore di così non la trovo più, perchè in questo mese mi hai illuminato le giornate, fin da subito mi hai dato consigli e mi sei stata vicino in molti momenti difficili, questo non è da tutti molti mi avrebbero mandato a fanculo molto tempo fa ma tu sei rimasta.
 
-Se mi chiedessero di te di sicuro andrei a dire che ti amo (ma tanto cazzo), che mi sono affezionato praticamente subito, che sei diventata una parte fondamentale della mia vita; eppure non riuscirei a spiegare le sensazioni che provo quando ti guardo negli occhi, è vero molte volte ti fisso e riconosco che sa un po da maniaco però dopo tutto resterei ore a guardarti, mi giustifico così; un’altra cosa che non riuscirei a spiegare e l’amore che provo quando ti soffermi sulle piccole cose tipo regalarmi la rosa piuttosto che la poesia, il peluches, il cuore (quello che hai fatto), ma anche le piccole occhiate che mi mandi ogni tanto, oppure preoccuparti se succede qualcosa o se torno a casa tardi in moto, ma anche semplicemente che sei interessata anche a una stronzata che ti sto dicendo… sono piccole cose ma sono quelle che fanno la differenza e la fanno soprattutto se le fai tu… 
+  Se mi chiedessero di te di sicuro andrei a dire che ti amo (ma tanto cazzo), che mi sono affezionato praticamente subito, che sei diventata una parte fondamentale della mia vita; eppure non riuscirei a spiegare le sensazioni che provo quando ti guardo negli occhi, è vero molte volte ti fisso e riconosco che sa un po da maniaco però dopo tutto resterei ore a guardarti, mi giustifico così; un’altra cosa che non riuscirei a spiegare e l’amore che provo quando ti soffermi sulle piccole cose tipo regalarmi la rosa piuttosto che la poesia, il peluches, il cuore (quello che hai fatto), ma anche le piccole occhiate che mi mandi ogni tanto, oppure preoccuparti se succede qualcosa o se torno a casa tardi in moto, ma anche semplicemente che sei interessata anche a una stronzata che ti sto dicendo… sono piccole cose ma sono quelle che fanno la differenza e la fanno soprattutto se le fai tu… 
 
-Questo solo per dirti che ti amo, e sono molto affezionato a te, anche se si è passato solo un mese, oltre a tutto questo non ho mai avuto una relazione come questa sono abituato a vederle finire male, ma mi fido di te e so che insieme possiamo passare sopra tutti e tutto, dato che siamo così competitivi; 
-Per finire vorrei dire che sei diventata una sorta di ossessione per me, sei sempre nei miei pensieri, di notte o di giorno, nei momenti belli o quelli più difficili… perciò si mi sa che ti tocca sopportarmi per un bel po… Ti amo tanto amore mio, più di quanto riesca a scrivere e più di quanto riesco a dirtelo.
+  Questo solo per dirti che ti amo, e sono molto affezionato a te, anche se si è passato solo un mese, oltre a tutto questo non ho mai avuto una relazione come questa sono abituato a vederle finire male, ma mi fido di te e so che insieme possiamo passare sopra tutti e tutto, dato che siamo così competitivi; 
+  Per finire vorrei dire che sei diventata una sorta di ossessione per me, sei sempre nei miei pensieri, di notte o di giorno, nei momenti belli o quelli più difficili… perciò si mi sa che ti tocca sopportarmi per un bel po… Ti amo tanto amore mio, più di quanto riesca a scrivere e più di quanto riesco a dirtelo.
 
-P.S. Sei una persona che pensa molto, a volte troppo, ti soffermi sulle minime cose, l’ho visto, ma sono qui anche per dirti che non devi lasciarti fermare da questa cosa sei la ragazza più forte che abbia mai visto so che puoi affrontare molte sfide e io sarò al tuo fianco e ti supporterò e se mi darai il permesso ti darò una mano e ne usciremo vittoriosi insieme.           
-`,
-"lista": `1. <a href="https://vm.tiktok.com/ZNdkKnxoq/" target="_blank">Cliccami</a><br>
-2. <a href="https://vm.tiktok.com/ZNdkKQ34u/" target="_blank">Cliccami</a><br>
-3. <a href="https://vm.tiktok.com/ZNdkK5RqF/" target="_blank">Cliccami</a><br>
-4. <a href="https://vm.tiktok.com/ZNdkKP9jt/" target="_blank">Cliccami</a><br>
-5. <a href="https://vm.tiktok.com/ZNdkKP9jt/" target="_blank">Cliccami</a><br>
-6. <a href="https://vm.tiktok.com/ZNdkKqTvQ/" target="_blank">Cliccami</a><br>
-7. <a href="https://vm.tiktok.com/ZNdkKbsa8/" target="_blank">Cliccami</a><br>
-8. <a href="https://vm.tiktok.com/ZNdkKfow2/" target="_blank">Cliccami</a><br>
-9. <a href="https://vm.tiktok.com/ZNdkKXdpA/" target="_blank">Cliccami</a><br>`
+  P.S. Sei una persona che pensa molto, a volte troppo, ti soffermi sulle minime cose, l’ho visto, ma sono qui anche per dirti che non devi lasciarti fermare da questa cosa sei la ragazza più forte che abbia mai visto so che puoi affrontare molte sfide e io sarò al tuo fianco e ti supporterò e se mi darai il permesso ti darò una mano e ne usciremo vittoriosi insieme.           
+  `,
+  "lista": `1. <a href="https://vm.tiktok.com/ZNdkKnxoq/" target="_blank">Cliccami</a><br>
+  2. <a href="https://vm.tiktok.com/ZNdkKQ34u/" target="_blank">Cliccami</a><br>
+  3. <a href="https://vm.tiktok.com/ZNdkK5RqF/" target="_blank">Cliccami</a><br>
+  4. <a href="https://vm.tiktok.com/ZNdkKP9jt/" target="_blank">Cliccami</a><br>
+  5. <a href="https://vm.tiktok.com/ZNdkKP9jt/" target="_blank">Cliccami</a><br>
+  6. <a href="https://vm.tiktok.com/ZNdkKqTvQ/" target="_blank">Cliccami</a><br>
+  7. <a href="https://vm.tiktok.com/ZNdkKbsa8/" target="_blank">Cliccami</a><br>
+  8. <a href="https://vm.tiktok.com/ZNdkKfow2/" target="_blank">Cliccami</a><br>
+  9. <a href="https://vm.tiktok.com/ZNdkKXdpA/" target="_blank">Cliccami</a><br>`,
+  "ricordo": `Un ricordo a cui non smettero mai di pensare e la giornata del picnic che abbiamo fatto, alla fine era una giornata semplice con poche idee messe insieme, ma se ci pensi e stata una giornata fantastica dalla cenacon il sushi, al tramonto rosso fuoco visto da distante, a quando hai provato la moto per la prima volta, tutte cose piccole ma che hanno reso la giornata indimenticabile, non che tutte le altre giornate con te non lo siano perchè sia chiaro, mi diverto un casino con te;
+  Quella giornata mi e rimasta impressa, perchè?, bhe perchè ero libero, con 0 pensieri ed ero con te perciò non mi preoccupavo neanche di quello che succcedeva sapevo cche se capitava qualcosa di storto lo avremmo risolto insieme, è se ci pensi e anche questa la nostra forza, ovvero quella di supportarci lun l'altro, se tu hai un problema anche io ce l'ho finchè non lo risolviamo, e so che dirai si e vero però alcune cose preferisco risolverle da sola, sono completamente daccordo, io non ho mai detto che posso risolvere tutti i problemi che verranno fuori ma sarò comunque li a supportarti e avrai sempre una persona su cui contare e quella persona sono IO
+  <div>
+    <img src="bacio.jpg" width="400px">
+    <img src="tramonto.jpg" width="400px">
+    <img src="insieme.jpg" width="400px">
+  </div> `
 };
 
 
